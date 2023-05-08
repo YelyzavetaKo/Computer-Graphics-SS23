5a61cf749974a21533fe8495dcce16b7af9c9a3f
YelyzavetaKo <yk66supa@studserv.uni-leipzig.de>
2023-05-08 12:04:44 +0200

    Codebasis

---
+++ build/CMakeFiles/CGPraktikum.dir/src/Color.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/CGPraktikum.dir/src/Color.cpp.o: \
+ /home/lisa/Documents/Uni/CG/Codebasis_Computergrafik_2023/src/Color.cpp \
+ /usr/include/stdc-predef.h \
+ /home/lisa/Documents/Uni/CG/Codebasis_Computergrafik_2023/src/Color.hpp
---
+++ src/WireframeRenderer.cpp
@@ -0,0 +1,29 @@
+
+#include "WireframeRenderer.hpp"
+
+/**
+** Zeichnet alle Dreiecke der Scene als Wireframe-Rendering unter Verwendung des
+* Bresenham-Algorithmus
+** Precondition: Sowohl mImage als auch mScene müssen gesetzt sein.
+** (Aufgabenblatt 2 - Aufgabe 1)
+**/
+void WireframeRenderer::renderScene(Color color) {
+}
+
+/**
+** Zeichnet unter Verwendung des Bresenham Algorithmus eine Linie zwischen p1
+* und p2 (nutzt x & y Komponente - z Komponente wird ignoriert)
+** Precondition: Das mImage muss gesetzt sein.
+** (Aufgabenblatt 1 - Aufgabe 2)
+**/
+void WireframeRenderer::drawBresenhamLine(GLPoint p1, GLPoint p2, Color color) {
+
+}
+
+/**
+** Füllt einen vorgegebenen Bereich (abgegrenzt durch Randfarbe/borderColor) mit einer vorgegebenen Farbe (fillColor).
+** Precondition: Das mImage muss gesetzt sein.
+** (Aufgabenblatt 1 - Aufgabe 3) 
+**/
+void WireframeRenderer::seedFillArea(GLPoint seed, Color borderColor, Color fillColor) {
+}
